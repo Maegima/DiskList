83e6e573ebfd8d04fab9f321b28103056862a647
maegima <andre.maegima@unifesp.br>
2023-09-07 16:15:00 -0300

    Changed to list files recursively

--- DiskList.cpp
+++ DiskList.cpp
@@ -14,18 +14,28 @@
 #include "FileInfo.hpp"
 
 using namespace std;
+ 
+list<FileInfo> getFileInfoList(filesystem::path path){
+    list<FileInfo> fileList;
+    for(auto const &entry : filesystem::directory_iterator{path}){
+        FileInfo fileInfo = FileInfo(entry);
+        fileList.push_back(fileInfo);
+        if(fileInfo.type == FileType::Directory){
+            list<FileInfo> dirList = getFileInfoList(entry.path());
+            fileList.insert(fileList.end(), dirList.begin(), dirList.end());
+        }
+    }
+    return fileList;
+}
 
 int main(int argc, char **argv){
     filesystem::path current_path = filesystem::current_path();
     if(argc > 1){
         current_path.append(argv[1]);
     }
-    list<FileInfo> fileList;
-    for(auto const &entry : filesystem::directory_iterator{current_path}){
-        fileList.push_back(FileInfo(entry));
-    }
+    list<FileInfo> fileList = getFileInfoList(current_path);
     for(auto file : fileList){
         cout << file << endl;
     } 
-    return 0;
+    return 0; 
 }
--- FileInfo.hpp
+++ FileInfo.hpp
@@ -20,13 +20,13 @@ enum FileType {Directory, File};
 class FileInfo {
 private:
     std::string path;
-    FileType type;
     off_t size;
     long created;
     long modified;
     long accessed;
     uint8_t* md5sum;
 public:
+    FileType type;
     FileInfo(std::filesystem::directory_entry entry);
     std::string size_to_string(size_t size) const;
     std::string to_string() const; 
