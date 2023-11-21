e00ad3b50cb7f2e96574e1132aacf76dc0a0cdb1
maegima <andre.maegima@unifesp.br>
2023-11-21 20:59:09 +0000

    Added unwind and organize functions

--- Application/Windows/CardPanel.cpp
+++ Application/Windows/CardPanel.cpp
@@ -3,30 +3,29 @@
  * @author André Lucas Maegima
  * @brief CardPanel class implementation
  * @version 0.3
- * @date 2023-11-20
- * 
+ * @date 2023-11-21
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #include "CardPanel.hpp"
 #include "ListingWindow.hpp"
 
-std::map<std::string, wxImage*> CardPanel::default_images;
+std::map<std::string, wxImage *> CardPanel::default_images;
 
 void CardPanel::InitializeDefaultIcons(std::map<std::string, std::string> config) {
-    for(auto &[key, value] : config){
+    for (auto &[key, value] : config) {
         default_images.insert({key, new wxImage(value, wxBITMAP_TYPE_PNG)});
     }
 }
 
-CardPanel::CardPanel(ListingWindow* parent, std::filesystem::directory_entry entry, wxString path) 
-    : wxPanel(parent, wxID_ANY), 
-    parent(parent),
-    file(FileInfo(entry, false)),
-    image(CreateImage(entry)), 
-    label(CreateLabel(entry, path))
-    {
+CardPanel::CardPanel(ListingWindow *parent, std::filesystem::directory_entry entry, wxString path)
+    : wxPanel(parent, wxID_ANY),
+      parent(parent),
+      file(FileInfo(entry, false)),
+      image(CreateImage(entry)),
+      label(CreateLabel(entry, path)) {
     auto sizer = new wxBoxSizer(wxVERTICAL);
     SetSizer(sizer);
     sizer->AddSpacer(5);
@@ -40,18 +39,18 @@ CardPanel::~CardPanel() {
     delete label;
 }
 
-wxStaticText* CardPanel::CreateLabel(std::filesystem::directory_entry entry, wxString path) {
+wxStaticText *CardPanel::CreateLabel(std::filesystem::directory_entry entry, wxString path) {
     wxString name = path.IsEmpty() ? wxString::FromUTF8(entry.path().filename()) : path;
     wxStaticText *text = new wxStaticText(this, wxID_ANY, name, wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
     int max_text_size = 180;
-    while(text->m_width > 200) {
-        int parts = text->m_width/max_text_size;
-        int size = name.length()/(parts + 1);
+    while (text->m_width > 200) {
+        int parts = text->m_width / max_text_size;
+        int size = name.length() / (parts + 1);
         wxString newstr = "";
-        for(int i = 0; i < parts; i++){
-            newstr += name.substr(i*size, size) + "\n";
+        for (int i = 0; i < parts; i++) {
+            newstr += name.substr(i * size, size) + "\n";
         }
-        newstr += name.substr(parts*size);
+        newstr += name.substr(parts * size);
         text->SetLabel(newstr);
         max_text_size -= 10;
     }
@@ -59,38 +58,36 @@ wxStaticText* CardPanel::CreateLabel(std::filesystem::directory_entry entry, wxS
     return text;
 }
 
-Image* CardPanel::CreateImage(std::filesystem::directory_entry entry) {
+Image *CardPanel::CreateImage(std::filesystem::directory_entry entry) {
     Image *img = nullptr;
     std::string extension = entry.path().extension().string();
     if (entry.is_directory()) {
-        img = new Image(this, default_images["folder"]);   
+        img = new Image(this, default_images["folder"]);
         img->Bind(wxEVT_LEFT_DOWN, &CardPanel::OnFolderLeftClick, this, wxID_ANY);
         img->Bind(wxEVT_RIGHT_DOWN, &CardPanel::OnFolderRightClick, this, wxID_ANY);
     } else if (extension == ".png") {
         wxString path = wxString::FromUTF8(entry.path());
         img = new Image(this, new wxImage(path, wxBITMAP_TYPE_PNG), Image::Type::DYNAMIC);
-    } 
-    else if(default_images.contains(extension)){
+    } else if (default_images.contains(extension)) {
         img = new Image(this, default_images[extension]);
     } else {
         img = new Image(this, default_images["default"]);
         img->Bind(wxEVT_LEFT_DOWN, &CardPanel::OnFileClick, this, wxID_ANY);
     }
     return img;
-    
 }
 
-void CardPanel::OnTextClick(wxMouseEvent& event) {
+void CardPanel::OnTextClick(wxMouseEvent &event) {
     std::cout << label->GetLabel() << "\n";
     event.Skip();
 }
 
-void CardPanel::OnFolderLeftClick(wxMouseEvent& event) {
+void CardPanel::OnFolderLeftClick(wxMouseEvent &event) {
     std::cout << label->GetLabel() << " " << parent->current << "\n";
     parent->ChangePath(file.path);
 }
 
-void CardPanel::OnFileClick(wxMouseEvent& event) {
+void CardPanel::OnFileClick(wxMouseEvent &event) {
     std::cout << label->GetLabel() << " " << parent->current << " file\n";
     std::list<std::pair<wxString, wxString>> list;
     list.push_back({"Name", file.path.filename().string()});
@@ -101,17 +98,98 @@ void CardPanel::OnFileClick(wxMouseEvent& event) {
     parent->iwindow->FillGrid(list);
 }
 
-bool CardPanel::CompareCards::operator() (const CardPanel* c1, const CardPanel* c2) const {
-    if(c1->file.type != c2->file.type)
+bool CardPanel::CompareCards::operator()(const CardPanel *c1, const CardPanel *c2) const {
+    if (c1->file.type != c2->file.type)
         return c1->file.type == FileType::Directory;
     return c1->file.path < c2->file.path;
 }
 
+void entry_move(const std::filesystem::path &entry, const Configuration &config) {
+    std::string extension = entry.extension().string();
+    std::filesystem::path path = entry.parent_path();
+    for (const auto &[key, list] : config.organize) {
+        if (std::find(list.begin(), list.end(), extension) != list.end()) {
+            path /= key;
+            break;
+        }
+    }
+    path /= entry.filename().c_str();
+    if (path != entry) {
+        if (!std::filesystem::exists(path.parent_path())) {
+            std::filesystem::create_directory(path.parent_path());
+        }
+        std::filesystem::rename(entry, path);
+    }
+}
+
+void organize_file(const std::filesystem::path &path, const Configuration &config) {
+    std::filesystem::path folder = path;
+    for (auto const &entry : std::filesystem::directory_iterator{path}) {
+        std::filesystem::path filename = entry.path().filename();
+        if (entry.is_regular_file()) {
+            try {
+                entry_move(entry, config);
+            } catch (std::filesystem::filesystem_error &e) {
+            }
+        } else if (entry.is_directory() && !config.organize.contains(filename)) {
+            organize_file(entry, config);
+        }
+    }
+}
+
+std::list<std::filesystem::path> get_files(const std::filesystem::path &path) {
+    std::list<std::filesystem::path> files;
+    for (auto const &entry : std::filesystem::directory_iterator{path}) {
+        if (entry.is_regular_file()) {
+            files.push_back(entry);
+        } else if (entry.is_directory()) {
+            files.splice(files.end(), get_files(entry));
+        }
+    }
+    return files;
+}
+
+std::list<std::filesystem::path> get_names(const std::filesystem::path &path) {
+    std::list<std::filesystem::path> names;
+    for (auto const &entry : std::filesystem::directory_iterator{path}) {
+        std::filesystem::path filename = entry.path().filename();
+        names.push_back(filename);
+    }
+    return names;
+}
+
+void unwind_files(const std::filesystem::path &path, const std::filesystem::path &parent) {
+    auto files_to_unwind = get_files(path);
+    auto current_names = get_names(parent);
+    for (auto const &file : files_to_unwind) {
+        int count = 1;
+        std::string stem = file.filename().stem();
+        std::string extension = file.filename().extension();
+        std::string filename = file.filename();
+        while (std::find(current_names.begin(), current_names.end(), filename) != current_names.end()) {
+            filename = stem + "(" + std::to_string(count++) + ")" + extension;
+        }
+        current_names.push_back(filename);
+        std::filesystem::path new_path = parent / filename;
+        try {
+            if (!std::filesystem::exists(new_path)) {
+                std::filesystem::rename(file, new_path);
+            } else {
+                wxLogError(wxString(new_path.string() + " exists!"));
+            }
+        } catch (std::filesystem::filesystem_error &e) {
+            wxLogError(wxString(filename + ": ") << e.what());
+        }
+    }
+}
+
 void CardPanel::OnFolderMenuClick(wxCommandEvent &evt) {
     switch (evt.GetId()) {
-        case FOLDER_UNWIND:     
+        case FOLDER_UNWIND:
+            unwind_files(this->file.path, this->file.path.parent_path());
             break;
         case FOLDER_ORGANIZE:
+            organize_file(this->file.path, this->parent->config);
             break;
     }
 }
